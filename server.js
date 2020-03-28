119fbbab1a522dcc01740fe62f6fcbe80d3cf23d
elinahedenius <bohedenius@protonmail.com>
2020-03-28 16:13:03 +0100

    nah ipfs was hard i'll go with mysql for now

--- server.js
+++ server.js
@@ -2,30 +2,27 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io').listen(http);
-const fs = require('fs');
-const IPFS = require('ipfs-mini');
+var mysql = require('mysql');
 
-//start an ipfs node
-const ipfs = new IPFS({
-  host: 'ipfs.infura.io',
-  port: 5001,
-  protocol: 'https'
+//create a mysql connection
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'docsthing',
+  password: 'testPassword',
+  database: 'docsthing'
 });
 
-//upload content to ipfs
-//const data = 'this is a cool string';
-//ipfs.add(data, (err, hash) => {
-//  if(err) throw err;
-//  console.log('https://ipfs.infura.io/ipfs/' + hash);
-//})
+//connect to database
+db.connect(function(err) {
+  if (err) throw err;
+  console.log('connected to database');
+});
 
-//start the server
-http.listen(2525, function () {
-  console.log('server stared on port 2525');
-})
-
+//start server
+http.listen(3000, function () {
+  console.log('server started on port 3000');
+});
 app.use(express.static(__dirname + '/public/'));
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -33,16 +30,10 @@ app.get('/editor', function(req, res){
   res.sendFile(__dirname + '/public/editor.html');
 });
 
-
 //socket.io
 io.sockets.on('connection', function(socket){
-
-  //save document
-  socket.on('save', (data) => {
+  //save
+  socket.on('save', function(data){
     console.log(data);
-    ipfs.add(data, (err, hash) => {
-      if(err) throw err;
-      console.log('https://ipfs.infura.io/ipfs/' + hash);
-    });
   });
 });
